6742ee76300a669b0c09b031b4f4946156ddfca4
muhammad-salem <salem.ebo@gmail.com>
2024-10-07 21:59:30 +0300

    TrustManager

--- src/main/java/org/injector/tools/ssh/proxyhandler/SniHostNameInjectionProxy.java
+++ src/main/java/org/injector/tools/ssh/proxyhandler/SniHostNameInjectionProxy.java
@@ -4,12 +4,13 @@ import com.jcraft.jsch.JSchException;
 import org.injector.tools.log.Logger;
 import org.injector.tools.speed.NetworkMonitorSpeed;
 
-import javax.net.ssl.SNIHostName;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 
 public class SniHostNameInjectionProxy extends ProxySocket {
@@ -25,7 +26,8 @@ public class SniHostNameInjectionProxy extends ProxySocket {
     public Socket openSocketConnection(String hostname, int port, int timeout) throws IOException, JSchException {
         var address = InetAddress.getByName(hostname);
         Logger.debug(getClass(),"Resolve Host name: [%s] with IP [%s]", hostname, address.toString());
-        var factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+//        var factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        var factory = this.getSSLSocketFactory();
         var socket = (SSLSocket) factory.createSocket(address.getHostAddress(), port);
         var serverName = new SNIHostName(this.sniHost);
         var params = socket.getSSLParameters();
@@ -33,6 +35,23 @@ public class SniHostNameInjectionProxy extends ProxySocket {
         socket.setSSLParameters(params);
         Logger.debug(getClass(),"Use SNI Host Name: %s", (Object) this.sniHost);
         return socket;
+    }
+
+    private SSLSocketFactory getSSLSocketFactory() {
+        var trustManagers = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return null; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+        };
+        try {
+            var sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustManagers, null);
+            return sslContext.getSocketFactory();
+        } catch (Exception e) {
+            return  (SSLSocketFactory) SSLSocketFactory.getDefault();
+        }
     }
 
 }
